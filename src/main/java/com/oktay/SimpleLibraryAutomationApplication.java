f34894c367fbcbcf208dc35f423c1e07b93223a8
oktayuyar <oktyuyar@gmail.com>
2017-09-29 14:24:13 +0300

    uygulamada düzenlemeler yapıldı

--- src/main/java/com/oktay/SimpleLibraryAutomationApplication.java
+++ src/main/java/com/oktay/SimpleLibraryAutomationApplication.java
@@ -1,21 +1,15 @@
 package com.oktay;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 /**
  * @author oktay
  *
  */
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration 
 public class SimpleLibraryAutomationApplication  extends SpringBootServletInitializer{
 
 	@Override
@@ -27,9 +21,4 @@ public class SimpleLibraryAutomationApplication  extends SpringBootServletInitia
 		SpringApplication.run(SimpleLibraryAutomationApplication.class, args);
 	}
 
-	@Bean
-	public HibernateJpaSessionFactoryBean sessionFactory() {
-	    return new HibernateJpaSessionFactoryBean();
-	}
-	
 }
